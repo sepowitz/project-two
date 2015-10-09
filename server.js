512d7f7dd99fa74f5d9b4ce726334f676ce0e111
Sebastian Odell <sebastianodell@Sebastians-MacBook-Air.local>
2015-10-09 11:11:39 -0400

    fixing MongoLab error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 		PORT = process.env.PORT || 3000,
 		server = express(),
-		MONGOURI = process.env.MONOLAB_URI || "mongodb://localhost:27017",
+		MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
 		dbname = "relevant_db_name",
 		mongoose = require('mongoose');
 
